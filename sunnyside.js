23a478ae3777c3d38535c9a149891e6563d02010
Balázs Suhajda <balazs@onereason.eu>
2012-01-31 15:47:03 +0100

    add all

---
+++ sunnyside.js
@@ -0,0 +1,24 @@
+
+
+var sunnyside = ( function () {
+
+	function init () {
+
+	}
+
+	function calculateAngles () {
+
+	}
+
+	function generateStyles () {
+
+	}
+
+	function setLocation ( l ) {
+
+	}
+
+	return {
+		setLocation: setLocation
+	};
+})();
